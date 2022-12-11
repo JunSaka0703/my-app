1842263a449ee02e84bd2698a72b32ad122bf941
Jace <67312955+JunSaka0703@users.noreply.github.com>
2022-12-12 01:15:14 +0900

    カバー画像設定

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { products } from 'src/app/products';
 
 @Component({
   selector: 'app-product-listings',
   templateUrl: './product-listings.component.html',
   styleUrls: ['./product-listings.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
 
-  products: any = [1, 2, 3, 4]
+  products: any
+
+  ngOnInit() {
+    this.products = products;
+  }
 }
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,24 +1,43 @@
 export const products = [
     {
+        coverImage: './assets/img/phone-cover.jpg',
         name: 'Phone XL',
         price: 799,
         description: 'A large phone with one of the best screens',
-        heading1: 'test'
+        heading1: 'head1 phone XL',
+        heading2: 'head2 phone XL',
+        heading3: 'head3 phone XL',
+        headingtext1: 'RouterModuleをエクスポートしたAppRoutingModuleをAppModuleがインポートしているので、 このRouterOutletは、このAppComponentで利用できるようになったルーターのディレクティブのひとつです。',
+        headingtext2: 'head text 2',
+        headingtext3: 'head text 3',
+
     },
     {
+        coverImage: './assets/img/phone-cover.jpg',
         name: 'Phone Mini',
         price: 699,
-        description: 'A great phone with one of the best cameras'
+        description: 'A great phone with one of the best cameras',
+        heading1: 'head1 phone Mini',
+        heading2: 'head2 phone Mini',
+        heading3: 'head3 phone Mini'
     },
     {
+        coverImage: './assets/img/phone-cover.jpg',
         name: 'Phone Standard',
         price: 299,
-        description: ''
+        description: 'A basic phone',
+        heading1: 'head1 phone Standard',
+        heading2: 'head2 phone Standard',
+        heading3: 'head3 phone Standard'
     },
 
     {
+        coverImage: './assets/img/phone-cover.jpg',
         name: 'Phone Special',
         price: 499,
-        description: ''
+        description: 'A special phone with one of the best performance',
+        heading1: 'head1 phone Special',
+        heading2: 'head2 phone Special',
+        heading3: 'head3 phone Special'
     }
 ];
