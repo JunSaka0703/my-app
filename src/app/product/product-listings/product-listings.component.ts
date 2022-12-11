2c76f10ffd0cb5f0bfbc39b778b727edd85d5a9b
Jace <67312955+JunSaka0703@users.noreply.github.com>
2022-12-11 19:31:03 +0900

    ProductModule追加

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.css']
+})
+export class ProductListComponent {
+
+  products: any = [1, 2, 3, 4]
+}
